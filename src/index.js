de6d16104aa88d00f93aa5875b873b08c0c99d0d
Jensen Bernard <jensenbernard5@gmail.com>
2017-03-14 21:39:44 +0100

    Update version

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 // @flow
 
-import { initialize, isAd } from 'is-ad';
+import { initialize, isAd, client } from 'is-ad';
 
 initialize();
 
 const defaultOptions = {
   verbose: true,
   logger: console,
+  onRequest: undefined,
 };
 
-export default (browserWindow: any, userOptions: Object = {}) => {
+export const adBlocker = client;
+
+export const blockWindowAds = (browserWindow: any, userOptions: Object = {}) => {
   const options = Object.assign({}, defaultOptions, userOptions);
   browserWindow.webContents.session.webRequest.onBeforeRequest(['*://*./*'], (details, callback) => {
     const shouldBeBlocked = isAd(details.url);
-    if (shouldBeBlocked) {
-      if (options.verbose) { options.logger.log(`ADBLOCK blocked: ${details.url}`); }
+    if (shouldBeBlocked && options.verbose) { options.logger.log(`ADBLOCK blocked: ${details.url}`); }
+    if (options.onRequest) {
+      options.onRequest(details, callback, shouldBeBlocked);
+    } else if (shouldBeBlocked) {
       callback({ cancel: true });
     } else {
       callback({ cancel: false });
