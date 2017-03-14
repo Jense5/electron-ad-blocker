de2c28049af95af138769d6660caf031c5cd97e0
Jensen Bernard <jensenbernard5@gmail.com>
2017-03-14 11:47:47 +0100

    Add function implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 // @flow
 
-export default {
-  add: (a: number, b: number) => a + b,
-  subtract: (a: number, b: number) => a - b,
+import { initialize, isAd } from 'is-ad';
+
+initialize();
+
+const defaultOptions = {
+  verbose: true,
+  logger: console,
+};
+
+export default (browserWindow: any, userOptions: Object = {}) => {
+  const options = Object.assign({}, defaultOptions, userOptions);
+  browserWindow.webContents.session.webResuest.onBeforRequest(['*://*./*'], (details, callback) => {
+    const shouldBeBlocked = isAd(details.url);
+    if (shouldBeBlocked) {
+      if (options.verbose) { options.logger.log(`ADBLOCK blocked: ${details.url}`); }
+      callback({ cancel: true });
+    } else {
+      callback({ cancel: false });
+    }
+  });
 };
